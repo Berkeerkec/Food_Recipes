e24b981a71d7a135ab11ddf4c3281fbf69f08e41
Berke Erkeç <berkeerkec0@gmail.com>
2023-06-15 00:02:05 +0300

    Navigation and bottomnavigaiton created.

--- app/src/main/java/com/berkeerkec/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/berkeerkec/foodrecipe/MainActivity.kt
@@ -2,10 +2,45 @@ package com.berkeerkec.foodrecipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.berkeerkec.foodrecipe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.recipesFoodFragment ->{
+                    navController.navigate(R.id.recipeFragment)
+                    true
+                }
+                R.id.favoriteRecipesFragment ->{
+                    navController.navigate(R.id.favoriteFragment)
+                    true
+                }
+                R.id.foodJokeFragment ->{
+                    navController.navigate(R.id.jokeFragment)
+                    true
+                }
+                else -> false
+
+            }
+        }
+
     }
 }
