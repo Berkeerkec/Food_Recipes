56d37aca835b7e9bbb1566d4fc0893074c08ef9e
Berke Erkeç <berkeerkec0@gmail.com>
2023-06-15 19:07:42 +0300

    Hilt has been integrated into the application and retrofit has been injected.

---
+++ app/src/main/java/com/berkeerkec/foodrecipe/hilt/AppModule.kt
@@ -0,0 +1,38 @@
+package com.berkeerkec.foodrecipe.hilt
+
+import com.berkeerkec.foodrecipe.api.FoodRecipesApi
+import com.berkeerkec.foodrecipe.util.Constant.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient() : OkHttpClient{
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient) : FoodRecipesApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(FoodRecipesApi::class.java)
+    }
+}
