a2e9bd980d44189543394bb74b849e58318e9941
Berke Erkeç <berkeerkec0@gmail.com>
2023-06-20 23:16:45 +0300

    Reorganized hilt library in build gradle.

--- app/src/main/java/com/berkeerkec/foodrecipe/view/MainActivity.kt
+++ app/src/main/java/com/berkeerkec/foodrecipe/view/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.berkeerkec.foodrecipe.R
 import com.berkeerkec.foodrecipe.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
