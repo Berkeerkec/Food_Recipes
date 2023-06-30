dd1b1328707dab9892dfb05878fa16e568e9e2d3
Berke Erkeç <berkeerkec0@gmail.com>
2023-06-30 13:45:13 +0300

    RecipesDao , RecipesDatabase and RecipesTypeConverter created.

--- app/src/main/java/com/berkeerkec/foodrecipe/room/RecipesDao.kt
+++ app/src/main/java/com/berkeerkec/foodrecipe/room/RecipesDao.kt
@@ -1,10 +1,11 @@
 package com.berkeerkec.foodrecipe.room
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
-
+@Dao
 interface RecipesDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
